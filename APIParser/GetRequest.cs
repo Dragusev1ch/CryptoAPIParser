e41acaf1af86ef27e51f2d38510729f0b934e1d5
Nazar <dmitriy@zala.com.a>
2023-02-10 09:53:00 +0200

    Створений GetRequest Створений PostRequest

---
+++ APIParser/GetRequest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIParser
+{
+    public class GetRequest
+    {
+        HttpWebRequest _request;
+        string _addres;
+
+        public Dictionary<string, string> Headers { get; set; }
+
+        public string Response { get; set; }
+        public string Accept { get; set; }
+        public string Host { get; set; }
+        public WebProxy Proxy { get; set; }
+
+        public GetRequest(string address)
+        {
+            _addres = address;
+            Headers = new Dictionary<string, string>();
+        }
+
+        public void Run()
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_addres);
+            _request.Method = "GET";
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+                if(stream != null) Response = new StreamReader(stream).ReadToEnd();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public void Run(CookieContainer cookieContainer)
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_addres);
+            _request.Method = "GET";
+            _request.CookieContainer = cookieContainer;
+            _request.Proxy = Proxy;
+            _request.Accept = Accept;
+            _request.Host = Host;   
+
+            foreach (var pair in Headers)
+            {
+                _request.Headers.Add(pair.Key, pair.Value);
+            }
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+                if (stream != null) Response = new StreamReader(stream).ReadToEnd();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
